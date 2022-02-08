47dc7a617ef354f5ad37f5a0c9aaf59a2c27e08f
christian blancheville <cblanche1858@student.landstede.nl>
2022-02-08 10:17:31 +0100

    Alles

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
