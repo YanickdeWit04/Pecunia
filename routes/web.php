31cac795189a699e894b98a9727e8d074d7beddc
christian blancheville <cblanche1858@student.landstede.nl>
2022-02-08 10:38:15 +0100

    Login blade aangemaakt met web.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('/welcome');
 });
-
