defd0dc6721e979cef0895031786c705dacf40b8
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-02-11 11:40:57 +0100

    titel + logo + stylesheet

--- resources/views/Overzicht.blade.php
+++ resources/views/Overzicht.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Overzicht</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-
+<div class="header">
+    <a href="/"><img src="/images/.png" alt="Logo" width="" height=""></a>
+    <h1>overzicht</h1>
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('passwords.login');
+Route::get('/overzicht', function () {
+    return view('overzicht');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
