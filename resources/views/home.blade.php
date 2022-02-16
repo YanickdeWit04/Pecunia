847b8a07498b2ad6e8153d5ada6840858a8d9fd6
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-02-16 12:12:02 +0100

    font toegevoegd en space gemaakt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Overzicht</title>
+    <title>Home</title>
     <link rel="stylesheet" href="/css/home.css">
 </head>
 <body>
 <div class="header">
-    <a href="/"><img src="/images/.png" alt="Logo" width="" height=""></a>
-    <h1>home</h1>
-</div>
-<div>
-    <form action="{{ route('logout') }}" method="post">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
+    <div class="linker-helft">
+        <a href="/"><img src="/images/.png" alt="Logo" width="" height=""></a>
+        <h2>home</h2>
+    </div>
+    <div class="rechter-helft">
+        <h2>Welkom {{ auth()->user()->name }}</h2>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit">Logout</button>
+        </form>
+    </div>
 </div>
 </body>
 </html>
