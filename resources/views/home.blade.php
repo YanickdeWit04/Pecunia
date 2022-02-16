bc4f3c6547b00020468ed9a12678a01c94992c7c
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-02-16 11:22:15 +0100

    home page gemaakt en logout knop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Overzicht</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/home.css">
 </head>
 <body>
 <div class="header">
     <a href="/"><img src="/images/.png" alt="Logo" width="" height=""></a>
-    <h1>overzicht</h1>
+    <h1>home</h1>
+</div>
+<div>
+    <form action="{{ route('logout') }}" method="post">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/overzicht', function () {
-    return view('overzicht');
+Route::get('/home', function () {
+    return view('home');
 });
-
+Route::post("/logout",[App\Http\Controllers\LogoutController::class,"store"])->name("logout");
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
