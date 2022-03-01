6c93b075f9a8333ac89f4316eaa6a7b4ba0fff3a
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-03-01 10:12:19 +0100

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html lang="NL">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
     <link rel="stylesheet" href="/css/home.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
+
 <body>
-<div class="header">
-    <div class="linker-helft">
-        <a href="/"><img src="/images/.png" alt="Logo" width="" height=""></a>
-        <h2>home</h2>
+    <div class="navbar">
+        <div class="linker-helft">
+            <a href="/"><img src="/images/beng.svg" alt="Logo" width="50px" height="50px"></a>
+            <h2 style="margin-top: 10px">home</h2>
+        </div>
+        <div class="display">
+            <div class="dropdown">
+                <button class="dropbtn">Welkom {{ auth()->user()->name }}</button>
+                <span class="material-icons">expand_more</span>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button class="logout-button logout-button-primary" type="submit">uitloggen</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="rechter-helft">
-        <h2>Welkom {{ auth()->user()->name }}</h2>
-        <form action="{{ route('logout') }}" method="post">
-            @csrf
-            <button type="submit">Logout</button>
-        </form>
-    </div>
-</div>
 </body>
+
 </html>
