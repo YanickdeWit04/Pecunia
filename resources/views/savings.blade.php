6a6e47c5217b4467d3850402a6e224bc727990f5
christian blancheville <cblanche1858@student.landstede.nl>
2022-03-11 10:25:05 +0100

    Niewe blade aan gemaakt Savings En het beginnen van maken van een spaar systeem

--- resources/views/savings.blade.php
+++ resources/views/savings.blade.php
@@ -15,20 +15,34 @@
 </header>
 <body>
 <div class="keuzes">
-    <table width="100%">
-        <tr>
-            <th>Spaar doelen gegevens</th>
-        </tr>
-
-        <td>Spaaren voor</td>
-        <td> Streefbedrag</td>
-        <td> Streefdatum</td>
-        <td> Begin Bedrag</td>
-        <td> Saving frequency<td/>
-        <td>Spaar locatie</td>
-        <td>Aanbetalings herinnering</td>
-
-    </table>
+    <h1> Saving goals menu </h1>
 </div>
+
+<div class="Saving form">
+    <form>
+        <label for="Voor">Spaaren voor:</label>
+        <input type="text" id="fname" name="fname" value=""><br><br>
+        <label for="streven">Streven voor:</label>
+        <input type="text" id="fname" name="fname" value=""><br><br>
+        <form action="https://example.com">
+            <label>
+                Start om spaardoelen te bereiken:
+                <input type="date" name="bday">
+            </label><br><br>
+            <label for="Begin">Startbedrag:<input type="number"></label><br><br>
+            <label for="start">Spaar rekening:</label>
+            <form>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                <label for="Rekening 1">Rekening 1</label>
+                <input type="radio" id="Rekening 1" name="fav_language" value="CSS">
+                <label for="Rekening 2">Rekening 2</label>
+                <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+                <label for="Rekening 3">Rekening 3</label><br><br>
+                <label for="Submit">Deposit reminder</label>
+                <input type="checkbox" id="submit" name="submit" value="submit"><br><br><br>
+                <input type="submit" value="Toevoegen">
+            </form>
+</div>
+
 </body>
 </html>
