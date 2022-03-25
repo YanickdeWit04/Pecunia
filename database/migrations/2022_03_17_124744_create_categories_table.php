89e90a5a1dd3b86eca9907093bd78360a7ee84db
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-03-25 11:25:42 +0100

    Categorie kan worden toegevoegd en geselecteerd worden bij de transactie toevoeg scherm

--- database/migrations/2022_03_17_124744_create_categories_table.php
+++ database/migrations/2022_03_17_124744_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             $table->string('name');
             $table->string('icon');
             $table->foreignId('user_id');
+            $table->timestamps();
         });
     }
 
