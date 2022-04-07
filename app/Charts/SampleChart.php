584d0535532333c826d1fc10393bfc53b77d047a
Student Yanick de Wit <ywit0068@student.landstede.nl>
2022-04-07 10:54:34 +0200

    Grafiek gewijzigd

--- app/Charts/SampleChart.php
+++ app/Charts/SampleChart.php
@@ -6,8 +6,11 @@ namespace App\Charts;
 
 use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
+use DateInterval;
+use DatePeriod;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SampleChart extends BaseChart
 {
@@ -18,17 +21,31 @@ class SampleChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
-        $data = Auth::user()->bankstatements()->orderBy('date', 'asc')->Where('amount', '>', '0')
-            ->groupBy('date')->selectRaw('sum(amount) as amount, date')->get();
-      //  json_encode($data);
-        $graphData = [];
-        foreach($data as $row){
-            if (isset($row['amount'])) {
-                $graphData[$row['date']] = $row['amount'];
-            };
-        }
+
+        $graphData = $this->balance();
         return Chartisan::build()
-            ->labels(array_keys($graphData))
-            ->dataset('inkomsten', array_values($graphData));
+            ->labels($graphData['labels'])
+            ->dataset('inkomsten', $graphData['values']);
+    }
+    public function balance($date = null){
+        $date = new \DateTime(now()->toString());
+        $date->modify('first day of this month');
+        $firstday = $date->format('Y-m-d');
+        $date->modify('last day of this month');
+        $lastday = $date->format('Y-m-d');
+        $period = new DatePeriod(
+            new \DateTime($firstday),
+            new \DateInterval('P1D'),
+            new \DateTime($lastday)
+        );
+
+        $dates = [];
+        $amounts = [];
+        foreach($period as $key => $value) {
+            $dates[] = $value->format('Y-m-d');
+            $amounts[] = DB::select("select sum(amount) as balance from bankstatements where date <= ?",[$value])[0]->balance;
+        }
+
+        return  ['labels'=>$dates,'values'=>$amounts];
     }
 }
